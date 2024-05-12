ef72c379780cc0912750bd30e574a7f79230c5c2
Kazuhiro <vladimir05032008@gmail.com>
2024-05-12 21:58:36 +0200

    My partfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMusic() {
+    let button = document.getElementById("music-button");
+    let audio = document.getElementById("bg-music");
+    if (audio.paused) {
+      audio.play();
+      button.innerHTML = "||";
+    } else {
+      audio.pause();
+      button.innerHTML = "/\\";
+    }
+  }
+  
+  document.addEventListener('mousemove', (e) => {
+      const object = document.getElementById('cursor_shade');
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+  
+      object.style.left = mouseX - (5*16) + 'px';
+      object.style.top = mouseY - (4*16) + 'px';
+  });
